7b0a33a91a687cc02990f769a0ff744162a7e789
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2018-01-20 03:48:11 -0500

    water removal

--- src/PostgresService.js
+++ src/PostgresService.js
@@ -4,10 +4,12 @@ const postgres = require(`pg-promise`);
  * Force all geometries to be the same winding order (for some reason they are not in DB)
  * Join them all in a single row.
  */
-const SQL_QUERY = `SELECT id,
- ST_AsGeoJSON(ST_Transform(way, 4326)) as data FROM 
+const SQL_QUERY_PREFIX = `SELECT id,
+ ST_AsGeoJSON(ST_Transform(ST_ForceRHR(way), 4326)) as data FROM
 (
-  SELECT id, ST_Multi(ST_Union(ST_ForceRHR(way))) AS way
+  SELECT id, ST_Multi(ST_Union(`;
+
+const SQL_QUERY_SUFFIX = `)) AS way
   FROM (
     SELECT tags->'wikidata' AS id, (ST_Dump(way)).geom AS way
     FROM $1~
@@ -15,6 +17,16 @@ const SQL_QUERY = `SELECT id,
     ) tbl1
   GROUP BY id
 ) tbl2`;
+
+const SQL_QUERY = `${SQL_QUERY_PREFIX}way${SQL_QUERY_SUFFIX}`;
+
+const SQL_QUERY_NO_WATER = `${SQL_QUERY_PREFIX}
+COALESCE(ST_Difference(
+  tbl1.way,
+  (select ST_Union(water.way) from $3~ water where ST_Intersects(tbl1.way, water.way))
+), tbl1.way)
+${SQL_QUERY_SUFFIX}`;
+
 
 class PostgresService {
 
@@ -45,12 +57,20 @@ class PostgresService {
    * Query geojson rows from Postgres
    * @param {string} table
    * @param {string[]} ids
+   * @param {{waterTable:string}} [opts]
    * @returns {{id:int, data:string}[]}
    */
-  query(table, ids) {
+  query(table, ids, opts) {
     if (ids.length === 0) return [];
 
-    return this._requester.query(SQL_QUERY, [table, ids]);
+    let query = SQL_QUERY;
+    const params = [table, ids];
+    if (opts && opts.waterTable) {
+      query = SQL_QUERY_NO_WATER;
+      params.push(opts.waterTable);
+    }
+
+    return this._requester.query(query, params);
   }
 
   /**
